cd5c28734d523d6be0b6e800517cc86f842b7362
david <david.svejda2000@gmail.com>
2023-09-20 14:32:52 +0200

    Continued modularisation and added a benchmark with communities that get created and destroyed

--- GraphGenerators.py
+++ GraphGenerators.py
@@ -6,25 +6,60 @@ def GirvanNewmanBenchmark(k_out, offset=0, density=1):
     k_in = 16 - k_out
     p_in = density * k_in / 31
     p_out = density * k_out / 96
-    pref_matrix = [
-        [p_in, p_out, p_out, p_out, p_in],
-        [p_out, p_in, p_out, p_out, p_out],
-        [p_out, p_out, p_in, p_out, p_out],
-        [p_out, p_out, p_out, p_in, p_out],
-        [p_in, p_out, p_out, p_out, p_in],
-    ]
     n_turns = offset // 32
     offset = offset % 32
 
+    communities = [(0, offset), (1, 32), (2, 32), (3, 32), (0, 32 - offset)]
+    communities = [((comm + n_turns) % 4, length) for comm, length in communities]
+
+    return stochasticBlockFromCummunities(
+        communities=communities, p_in=p_in, p_out=p_out
+    )
+
+
+def TestCreationDestruction(k_out, offset, density=1):
+    k_in = 16 - k_out
+    p_in = density * k_in / 31
+    p_out = density * k_out / 96
+    n_turns = offset // 32
+    offset = offset % 32
+
+    communities = [
+        (0, offset),
+        (1, 32),
+        (2, 32 - offset),
+        (3, offset),
+        (4, 32),
+        (5, 32 - offset),
+    ]
+    communities = [((comm + n_turns) % 6, length) for comm, length in communities]
+
+    return stochasticBlockFromCummunities(
+        communities=communities, p_in=p_in, p_out=p_out
+    )
+
+
+def stochasticBlockFromCummunities(communities, p_in, p_out):
+    pref_matrix = [
+        [(p_in if com1 == com2 else p_out) for com2, _ in communities]
+        for com1, _ in communities
+    ]
+
+    block_sizes = [length for _, length in communities]
+
     G = ig.Graph.SBM(
-        n=128,
+        n=sum(block_sizes),
         pref_matrix=pref_matrix,
-        block_sizes=[offset, 32, 32, 32, 32 - offset],
+        block_sizes=block_sizes,
         directed=False,
         loops=False,
     )
-    communities = [0] * offset + [1] * 32 + [2] * 32 + [3] * 32 + [0] * (32 - offset)
-    G.vs["community"] = [(community + n_turns) % 4 for community in communities]
+
+    comm_vector = []
+    for comm, length in communities:
+        comm_vector += [comm] * length
+
+    G.vs["community"] = comm_vector
     return G
 
 
@@ -55,20 +90,11 @@ def mergeGraphs(Graph1, Graph2):
     if "weight_1" in Graph2.es.attributes():
         del Graph2.es["weight_1"]
 
-    # print(Graph1.es.attributes())
-    # print(Graph1.vs.attributes())
-    # print(Graph1.attributes())
     G = ig.union([Graph1, Graph2])
-    # print(f"Graph 1 has {len(Graph1.vs)} vertices and {len(Graph1.es)} edges")
-    # print(f"Graph 2 has {len(Graph2.vs)} vertices and {len(Graph2.es)} edges")
-    # print(f"Graph G has {len(G.vs)} vertices and {len(G.es)} edges")
-    # print(G.vs.attributes())
-    # print(G.es.attributes())
     G.es["weight"] = [
         addWeights(weight_1, weight_2)
         for weight_1, weight_2 in zip(G.es["weight_1"], G.es["weight_2"])
     ]
-    # print(G.es["weight"])
     return G
 
 
@@ -102,4 +128,8 @@ def concatGraphs(graphs, weight):
 if __name__ == "__main__":
     print("Testing GraphGenerators")
 
-    mergeGraphs(GirvanNewmanBenchmark(6, 0), GirvanNewmanBenchmark(6, 16))
+    GirvanNewmanBenchmark(6, 16)
+    GirvanNewmanBenchmark(6, 48)
+
+    print()
+    TestCreationDestruction(6, 16)
--- TestMerging.py
+++ TestMerging.py
@@ -14,49 +14,11 @@ def calcDifference(partition, previous):
     return 1 - grAn.calculateAccuracy(partition, previous)[0]
 
 
-# Parameters
-n_steps = 2**4
-n_turns = 2
-k_out = 7
-
-
-if n_steps > 32 * n_turns:
-    raise ValueError(f"At most {32*n_turns} steps are allowed, not {n_steps}")
-step_size = 32 * n_turns // n_steps
-
-
 def generateGraphSequence(n_steps, step_size, generator, **kwargs):
     graphs = [None] * n_steps
     for i in range(n_steps):
         graphs[i] = generator(offset=i * step_size, **kwargs)
     return graphs
-
-
-# Generating the graphs
-graphs = generateGraphSequence(
-    n_steps, step_size, grGen.GirvanNewmanBenchmark, k_out=k_out, density=0.3
-)
-
-ks_stacked = [1, 2, 3, 4, 6, 8, 12, 16, 32]
-ks_time_sequence = [
-    0.1,
-    0.05,
-    0.02,
-    0.01,
-    0.007,
-    0.005,
-    0.004,
-    0.003,
-    0.0025,
-    0.002,
-    0.0015,
-    0.001,
-    0.0007,
-    0.0005,
-    0.0002,
-    0.0001,
-    0,
-]
 
 
 def measureClusteringMethod(graphs, clustering_func, ks):
@@ -71,12 +33,13 @@ def measureClusteringMethod(graphs, clustering_func, ks):
         partitions = clustering_func(graphs, k)
         for i in range(n_steps):
             modularity = graphs[i].modularity(partitions[i])
-            print(f"Modularity on G{i}: {modularity}")
+            # print(f"Modularity on G{i}: {modularity}")
             mod_sum += modularity
             if i > 0:
                 consistency_sum += grAn.Consistency(partitions[i], partitions[i - 1])
 
         modularities.append(mod_sum / n_steps)
+        print(f"Modularity average = {mod_sum/n_steps}")
         mod_sum = 0
         consistencies.append(consistency_sum / (n_steps - 1))
         consistency_sum = 0
@@ -94,36 +57,93 @@ def measureClusteringMethod(graphs, clustering_func, ks):
     return mods, consists
 
 
-# Refference point
-mod_sum = 0
-consistency_sum = 0
-for i in range(n_steps):
-    partition = graphs[i].vs["community"]
-    modularity = graphs[i].modularity(partition)
-    print(f"Modularity on G{i}: {modularity}")
-    mod_sum += modularity
-    if i > 0:
-        consistency_sum += grAn.Consistency(partition, previous_part)
-    previous_part = partition
+def ClusterTest(GenerationPars, clusterers, title, filename):
+    # Generating the graphs
+    graphs = generateGraphSequence(**GenerationPars)
+    # Refference point
+    mod_sum = 0
+    consistency_sum = 0
+    for i in range(n_steps):
+        partition = graphs[i].vs["community"]
+        modularity = graphs[i].modularity(partition)
+        mod_sum += modularity
+        if i > 0:
+            consistency_sum += grAn.Consistency(partition, previous_part)
+        previous_part = partition
+    modularity = mod_sum / n_steps
+    consistency = consistency_sum / (n_steps - 1)
+    print(f"Refference modularity: {modularity}")
 
-# Plotting
-fig, ax = plt.subplots(1, 1)
-mods, consists = measureClusteringMethod(graphs, grCls.clusterStacked, ks_stacked)
-ax.plot(consists, mods, "bo-", label="Merge-partition")
-mods, consists = measureClusteringMethod(
-    graphs, grCls.clusterConnected, ks_time_sequence
-)
-ax.plot(consists, mods, "go-", label="Connected-partition")
-ax.plot(
-    consistency_sum / (n_steps - 1),
-    mod_sum / n_steps,
-    "ro",
-    label="'True' community structure",
-)
-ax.set_ylim(bottom=0)
-ax.set_xlim(left=0)
-ax.set_ylabel("Average modularity")
-ax.set_xlabel("Average consistency")
-ax.legend()
-ax.set_title("Girvan and Newman benchmark with two turns in 16 steps")
-fig.savefig("plot.pdf")
+    # Plotting
+    fig, ax = plt.subplots(1, 1)
+    for clusterer in clusterers:
+        mods, consists = measureClusteringMethod(
+            graphs, clusterer["method"], clusterer["ks"]
+        )
+        ax.plot(consists, mods, "o-", label=clusterer["label"])
+    ax.plot(
+        consistency,
+        modularity,
+        "ro",
+        label="'True' community structure",
+    )
+    ax.set_ylim(bottom=0)
+    ax.set_xlim(left=0)
+    ax.set_ylabel("Average modularity")
+    ax.set_xlabel("Average consistency")
+    ax.legend()
+    ax.set_title(title)
+    fig.savefig(filename)
+
+
+# Parameters
+n_steps = 2**5
+n_turns = 2
+k_out = 7
+
+if n_steps > 32 * n_turns:
+    raise ValueError(f"At most {32*n_turns} steps are allowed, not {n_steps}")
+step_size = 32 * n_turns // n_steps
+
+GenerationPars = {
+    "n_steps": n_steps,
+    "step_size": step_size,
+    "generator": grGen.TestCreationDestruction,
+    "k_out": k_out,
+    "density": 0.5,
+}
+clusterers = [
+    {
+        "method": grCls.clusterStacked,
+        "ks": [1, 2, 3, 4, 6, 8, 12, 16, 32],
+        "label": "Merge-partition",
+    },
+    {
+        "method": grCls.clusterConnected,
+        "ks": [
+            0.1,
+            0.05,
+            0.02,
+            0.01,
+            0.007,
+            0.005,
+            0.004,
+            0.003,
+            0.0025,
+            0.002,
+            0.0015,
+            0.001,
+            0.0007,
+            0.0005,
+            0.0002,
+            0.0001,
+            0,
+        ],
+        "label": "Connected-partition",
+    },
+]
+
+title = "Girvan and Newman benchmark with two turns in 16 steps"
+filename = "plotCreationDestructionBenchmark.pdf"
+
+ClusterTest(GenerationPars, clusterers, title, filename)
